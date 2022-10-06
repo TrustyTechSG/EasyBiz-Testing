a0a854636296a6e7b17b1547e0541b57493e50f5
TrustyTech <trustytech.master@gmail.com>
2022-10-06 13:05:26 +0800

    restructrue

--- tests/pos/clockin-type.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import clockIn from './clockIn';
-
-test.beforeEach(clockIn);
-
-test('test', async ({ page }) => {
-  await expect(page.locator(`button:has-text("${process.env.STAFF_NAME}")`)).toBeEnabled();
-});
---
+++ tests/pos/clockin.spec.js
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+import clockIn from './clockIn';
+
+test.describe('Clock in', () => {
+  test('by click', async ({ page }) => {
+    // Go to https://stagingpos.easybus.app/?token=
+    await page.goto(`https://stagingpos.easybus.app/?token=${process.env.MAINTOKEN}`);
+
+    await expect(page.locator('text=Passcode')).toHaveText("Passcode");
+
+    // Click div[role="dialog"] button:has-text("0")
+    await page.locator('div[role="dialog"] button:has-text("0")').click();
+
+    // Click div[role="dialog"] button:has-text("0")
+    await page.locator('div[role="dialog"] button:has-text("0")').click();
+
+    // Click button:has-text("9")
+    await page.locator('button:has-text("9")').click();
+
+    // Click button:has-text("7")
+    await page.locator('button:has-text("7")').click();
+
+    // Click button:has-text("MAIN")
+    await expect(page.locator(`button:has-text("${process.env.STAFF_NAME}")`)).toBeEnabled();
+  });
+
+  test('by keyboard', clockIn);
+});
