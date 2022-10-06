ae6b5380b5a9ebcc52896a0ba6385e95983ee2c7
TrustyTech <trustytech.master@gmail.com>
2022-10-06 14:35:07 +0800

    dev

--- playwright.config.js
+++ playwright.config.js
@@ -37,6 +37,7 @@ const config = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
