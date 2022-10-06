77b63c14f729432780f748bef8cac53a78c30cf0
TrustyTech <trustytech.master@gmail.com>
2022-10-06 13:30:05 +0800

    dev

---
+++ tests/pos/customer-account.spec.js
@@ -0,0 +1,48 @@
+import { expect, test } from '@playwright/test';
+import clockIn from './clockIn';
+
+test.beforeEach(clockIn);
+
+test.describe('Customer account', () => {
+  const accounts = [
+    {
+      title: 'With name',
+      name: 'Test',
+      tel: '91231232',
+      email: 'test@gmail.com'
+    }
+  ];
+
+  for (const { title, name, tel, email } of accounts) {
+    test(title, async ({ page }) => {
+      // Click div:nth-child(3) > div > div > .flex > .ant-space > .ant-space-item > .ant-btn
+      await page.locator('div:nth-child(3) > div > div > .flex > .ant-space > .ant-space-item > .ant-btn').click();
+
+      if (name) {
+        // Click input[type="text"] >> nth=1
+        await page.locator('input[type="text"]').nth(1).click();
+        // Fill input[type="text"] >> nth=1
+        await page.locator('input[type="text"]').nth(1).fill(name);
+      }
+
+      if (tel) {
+        // Click [placeholder="\31  \(702\) 123-4567"]
+        await page.locator('[placeholder="\\31  \\(702\\) 123-4567"]').click();
+        // Fill [placeholder="\31  \(702\) 123-4567"]
+        await page.locator('[placeholder="\\31  \\(702\\) 123-4567"]').fill('+65 9988-98776');
+      }
+
+      if (email) {
+        // Click [placeholder="example\@gamil\.com"]
+        await page.locator('[placeholder="example\\@gamil\\.com"]').click();
+        // Fill [placeholder="example\@gamil\.com"]
+        await page.locator('[placeholder="example\\@gamil\\.com"]').fill('test@gmail.com');
+        // Click button:has-text("Create account")
+      }
+
+      await page.locator('button:has-text("Create account")').click();
+
+      await expect(page.locator(`text:has-text("${name}")`)).toBeVisible();
+    });
+  }
+});
