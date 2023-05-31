87a90454f61af856975218ac9631f65aa9f05ecf
Aswini <aswini@easybizcloud.com>
2023-05-31 14:46:22 +0530

    update

---
+++ tests/Acessory.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+ //Acessory
+test('test', async ({ page }) => {
+  const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
+  await page.goto(response.url);
+  await page.getByText('Windbreaker').click();
+  await page.getByRole('heading', { name: 'Laundry' }).getByRole('button', { name: 'plus' }).click();
+  await page.getByRole('button', { name: 'Line item details' }).click();
+  await page.getByRole('button', { name: 'Accessory' }).click();
+  await page.getByRole('listitem').filter({ hasText: 'Own Hanger0 pcs' }).getByRole('button', { name: 'plus' }).click();
+  const locator = page.getByText('Accessory: Own Hanger ');
+  await expect(locator).toBeVisible();
+
+});
---
+++ tests/Add on.js
@@ -0,0 +1,14 @@
+//Add on
+import { test, expect } from '@playwright/test';
+test('test', async ({ page }) => {
+  const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
+  await page.goto(response.url);
+  await page.getByText('T-Shirt').click();
+  await page.getByRole('heading', { name: 'Laundry' }).getByRole('button', { name: 'plus' }).click();
+  await page.getByRole('button', { name: 'right Line item details' }).click();
+  await page.getByRole('button', { name: 'ubnode Add-on' }).click();
+  await page.getByRole('listitem').filter({ hasText: 'HANG STRAIGHT[H/S]' }).getByRole('button', { name: 'plus' }).click();
+  const locator = page.getByText('Add-on: HANG STRAIGHT');
+  await expect(locator).toBeVisible();
+  // At receipt page
+});
