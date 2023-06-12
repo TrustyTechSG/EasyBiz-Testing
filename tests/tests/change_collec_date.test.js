87b4ff1f16ddb3d7102dcaeccfc0ad8a8bfaeffe
Aswini <aswini@easybizcloud.com>
2023-06-12 16:03:33 +0530

    update

--- tests/tests/change_collec_date.test.js
+++ tests/tests/change_collec_date.test.js
@@ -4,7 +4,7 @@ test('test', async ({ page }) => {
   const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
   await page.goto(response.url);
   await page.getByText('Sweater').click();
-  await page.getByRole('heading', { name: 'Laundry ₹6.00 minus 0 pcs plus' }).getByRole('button', { name: '0 pcs' }).click();
+  await page.getByRole('heading', { name: 'Laundry' }).getByRole('button', { name: '0 pcs' }).click();
   await page.getByRole('button', { name: '1', exact: true }).click();
   await page.getByRole('button', { name: 'Add' }).click();
   await page.getByPlaceholder('Search customer, order').click();
