19adff3bfe5e2dd020439c0d65ce882daad6c836
Aswini <aswini@easybizcloud.com>
2023-06-13 09:06:12 +0530

    update

--- tests/tests/Acessory.test.js
+++ tests/tests/Acessory.test.js
@@ -5,7 +5,6 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
   await page.goto(response.url);
-  //await page.getByText('Windbreaker').click();
   await page.locator('div').filter({ hasText: /^Windbreaker$/ }).nth(2).click();
   await page.getByRole('button', { name: 'plus' }).click();
   await page.getByRole('button', { name: 'Line item details' }).click();
