0e7cddb359b7064b5aa081e5aaabdfaf72b4508c
Aswini <aswini@easybizcloud.com>
2023-06-13 13:56:30 +0530

    update

--- tests/tests/internal_redo_receipt.test.js
+++ tests/tests/internal_redo_receipt.test.js
@@ -9,12 +9,11 @@ test('test', async ({ page }) => {
   await page.getByText('Business Jacket').click();
   await page.getByRole('heading', { name: 'Dry Clean' }).getByRole('button', { name: 'plus' }).click();
   await page.getByRole('button', { name: 'Payment' }).click();
-  await page.getByRole('button', { name: 'unpaid' }).click();
+  await page.getByRole('button', { name: 'cash' }).click();
   await page.getByRole('button', { name: 'Create order' }).click();
  await page.locator('label:nth-child(6) > .ant-segmented-item-label').click();
  await page.getByRole('button', { name: 'redo Redo' }).click();
  await page.getByRole('button', { name: 'Create redo order' }).click();
- await page.getByRole('button', { name: 'border #1' }).click();
- await page.getByRole('button', { name: 'Create redo order' }).click();
-
-})
+ await page.getByRole('tab', { name: 'Internal receipt' }).click();
+ await expect(page.getByRole('tabpanel', { name: 'Internal receipt' }).getByText('Subtotal ₹ 0.00')).toBeTruthy();
+ })
