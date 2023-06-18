7aa722842f26a71ef1be9619c4d9aa3f7e148111
Aswini <aswini@easybizcloud.com>
2023-06-18 14:49:28 +0530

    update

--- tests/POS_testscripts/order/discharge.test.js
+++ tests/POS_testscripts/order/discharge.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-//discharge
+
 test('test', async ({ page }) => {
 const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
 await page.goto(response.url);
@@ -10,6 +10,8 @@ await page.getByRole('heading', { name: 'Laundry' }).getByRole('button', { name:
 await page.getByRole('button', { name: ' Payment' }).click();
 await page.getByRole('button', { name: 'Cash' }).click();
 await page.getByRole('button', { name: 'Create order' }).click();
-await page.getByRole('dialog').getByRole('superscript').click();
-});
-//await expect(page.getByText('Collection Receipt', { exact: true })).toBeTruthy;//
+await page.locator('.ant-segmented-item-icon > .ant-badge > .anticon > svg').click();
+await page.getByLabel('Select all').check();
+await page.getByRole('button', { name: 'check Discharge (1/1)' }).click();
+await page.getByRole('button', { name: 'OK', exact: true }).click();
+await expect(page.getByText('Collection Receipt', { exact: true })).toHaveText;});
