b8232f7e7c9213e86e5b4cf1df76063624b25583
Aswini <aswini@easybizcloud.com>
2023-06-21 11:48:11 +0530

    update

---
+++ tests/POS_testscripts/order/end__drawer.test.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+//end drawer
+test('test', async ({ page }) => {
+const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
+await page.goto(response.url);
+await page.getByRole('button', { name: 'container' }).click();
+await page.getByRole('button', { name: 'container' }).click();
+await page.getByRole('button', { name: 'container' }).click();
+await page.getByRole('button', { name: 'End drawer' }).click();
+await expect(page.getByRole('button', { name: 'Confirm end drawer' })).toHaveText;
+});
