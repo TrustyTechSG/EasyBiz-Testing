ac5bd745c180a07ff8852dcfcc5234b20a64a65a
Aswini <aswini@easybizcloud.com>
2023-07-05 12:37:29 +0530

    update

--- tests/item_selection/turn_off_active promo.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-//Disable the active promotion
-test('test', async ({ page }) => {
-  const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
-  await page.goto(response.url);
-  await page.getByRole('switch', { name: 'Enabled Disabled' }).click();
-  await expect(page.getByText('anniversary offer(2% off)')).toBeHidden();
-});
--- tests/order/min_order.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-//minimum order
-test('test', async ({ page }) => {
-const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
-await page.goto(response.url);
-expect(page.getByText('Minimum order ₹10.00')).toBeTruthy();
-
-});
