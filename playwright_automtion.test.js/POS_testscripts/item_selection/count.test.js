323e43f7aef317cf4421fc3d0c6e64f59b611ce8
Aswini <aswini@easybizcloud.com>
2023-07-05 12:25:21 +0530

    update

--- playwright_automtion.test.js/POS_testscripts/item_selection/count.test.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-//count
-test('test', async ({ page }) => {
-  const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
-  await page.goto(response.url)
-  await page.getByText('Blouse').click();
-  await page.getByRole('heading', { name: 'Laundry' }).getByRole('button', { name: 'plus' }).click();
-  await expect(page.getByTitle('1')).toBeVisible();
-});
