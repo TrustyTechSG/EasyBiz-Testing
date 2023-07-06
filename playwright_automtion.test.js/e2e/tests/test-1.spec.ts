9acce5b0785c93c682e611da8e3b5d24e77e1d44
Aswini <aswini@easybizcloud.com>
2023-07-06 11:55:47 +0530

    update

--- playwright_automtion.test.js/e2e/tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-// login
-import { test, expect } from '@playwright/test';
-test('test', async ({ page }) => {
-const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
-await page.goto(response.url);
-await expect(page.getByRole('button', { name: 'shop Thanjavur store' })).toBeTruthy();
-})
--- playwright_automtion.test.js/e2e/tests/test-14.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-//bag service (switch busiess)
-test('test', async ({ page }) => {
-  const response = await (await fetch(`https://us-central1-easybus-clean-cloud.cloudfunctions.net/testing?realm=JewIJxIl06qRVrOnhBL9&code=store1&method=pos_sign_in`)).json();
-  await page.goto(response.url);
-  await page.locator('.ant-space-item > .ant-btn').first()
-  await page.getByRole('heading', { name: 'Bag Service'}
-  )})
